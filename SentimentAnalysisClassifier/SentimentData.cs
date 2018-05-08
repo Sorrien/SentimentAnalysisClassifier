1d175c399c2b05f962ce3ba87d8e9e5beb015fdb
Sparks, Collin <collin.sparks@aarons.com>
2018-05-08 14:14:14 -0400

    Init

---
+++ SentimentAnalysisClassifier/SentimentData.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.ML.Runtime.Api;
+
+namespace SentimentAnalysisClassifier
+{
+    public class SentimentData
+    {
+        [Column(ordinal: "0")]
+        public string SentimentText;
+        [Column(ordinal: "1", name: "Label")]
+        public float Sentiment;
+    }
+}
